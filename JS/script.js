9a8ab1d962617ce19f94a1013846c3d0105d786c
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-21 17:54:17 +0200

    creato il timer(per rimuovere i numeri dalla pagina

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,31 @@
 //? Variabili del DOM//
-const StartButton=document.querySelector("section#start");
+const StartButton=document.querySelector("#start");
 const container=document.querySelector("#container");
 
 //% Generatore di Numeri casuali//
 
+StartButton.addEventListener('click', function(){
+    StartButton.classList.add('none');
+    let div=document.createElement('div');
+    container.append(div);
 
-for( i=0; i<5; i++ ){
-   let num= Math.floor(Math.random()*5);
-     let span=document.createElement('span');
+
+    for( i=0; i<5; i++ ){
+ let num= Math.floor(Math.random()*5);
+    let span= document.createElement('span');
+    div.append(span);
      span.append(num);
-     container.append(span);
+    
 
 }
 
+//> Creazione timer
+
+let timer= setInterval(function (){
+
+div.classList.add('none');
+
+
+
+},3000)
+})

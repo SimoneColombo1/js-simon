a654166590f8ffe196d9f65f0a86399871c6e687
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-21 17:37:50 +0200

    creato il timer

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
 //? Variabili del DOM//
 const StartButton=document.querySelector("section#start");
 const container=document.querySelector("#container");
+
+//% Generatore di Numeri casuali//
+
+
+for( i=0; i<5; i++ ){
+   let num= Math.floor(Math.random()*5);
+     let span=document.createElement('span');
+     span.append(num);
+     container.append(span);
+
+}
+
